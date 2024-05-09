0a52ed828c2cfd1ee75e0efc12af12da016ca190
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2024-05-09 18:06:00 +0300

    rewrite module1-2

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,11 +1,13 @@
+#!/usr/bin/env node
 import { CLIApplication } from './cli/cli-application.js';
-import { HelpCommand, VersionCommand } from './cli/index.js';
+import { HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
 
 function bootstrap () {
   const cliApplication = new CLIApplication();
   cliApplication.registerCommands([
     new HelpCommand(),
-    new VersionCommand()
+    new VersionCommand(),
+    new ImportCommand()
   ]);
 
   cliApplication.processCommand(process.argv);
