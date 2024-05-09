8810298c530146e85d9ace25782670a1e486d75b
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2024-05-09 16:35:26 +0300

    rewrite module1

---
+++ src/main.cli.ts
@@ -0,0 +1,14 @@
+import { CLIApplication } from './cli/cli-application.js';
+import { HelpCommand, VersionCommand } from './cli/index.js';
+
+function bootstrap () {
+  const cliApplication = new CLIApplication();
+  cliApplication.registerCommands([
+    new HelpCommand(),
+    new VersionCommand()
+  ]);
+
+  cliApplication.processCommand(process.argv);
+}
+
+bootstrap();
